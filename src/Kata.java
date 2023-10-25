55b569a3c184bdaacd7648b66c4c037eecebf9b3
Myron Holmes <1dequanh@gmail.com>
2023-10-25 11:52:59 -0400

    inital commit

---
+++ src/Kata.java
@@ -0,0 +1,15 @@
+public class Kata {
+
+//    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+    public static int makeNegative(final int x) {
+
+        if (x > 0){
+            return x * -1;
+        }else if (x <= 0 ){
+            return x;
+        }
+
+        return x;
+    }
+
+}
